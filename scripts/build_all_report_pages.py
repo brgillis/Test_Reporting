1a51716fbb2013044dc81a45aba80ab007cb55d1
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2022-10-11 15:47:19 +0100

    Adding stub script for building report pages

---
+++ scripts/build_all_report_pages.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+"""
+:file: build_all_report_pages.py
+
+:date: 10/11/2022
+:author: Bryan Gillis
+
+This python script is run to construct report pages for all validation tests. It reads in the file manifest, and then
+calls appropriate functions to construct pages for each test results xml file.
+"""
+
+# Copyright (C) 2012-2020 Euclid Science Ground Segment
+#
+# This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General
+# Public License as published by the Free Software Foundation; either version 3.0 of the License, or (at your option)
+# any later version.
+#
+# This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License along with this library; if not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+from argparse import ArgumentParser
+
+
+def main():
+    """Standard entry-point function for this script.
+    """
+
+    parser = ArgumentParser()
+
+    args = parser.parse_args()
+
+
+if __name__ == "__main__":
+    main()

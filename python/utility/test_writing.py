d8163dabac32fa1bd54a45cbb0623c674931ade9
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2022-10-17 16:23:42 +0100

    Implementing top-level functions for writing test summary

--- python/utility/test_writing.py
+++ python/utility/test_writing.py
@@ -26,8 +26,14 @@ from __future__ import annotations
 
 import codecs
 import hashlib
+import os
+import shutil
 from logging import getLogger
 from typing import Callable, Dict, List, NamedTuple, Optional, Union
+
+from utility.constants import DATA_DIR
+
+TMPDIR_MAXLEN = 16
 
 logger = getLogger(__name__)
 
@@ -103,10 +109,106 @@ class TestSummaryWriter:
 
         Returns
         -------
-        l_test_names_and_filenames : List[SummaryWriteOutput]
+        l_summary_write_output : List[SummaryWriteOutput]
             A list of objects, each containing the test name and filename and a list of the same for associated
             tests. If the input `value` is a filename, this will be a single-element list. If the input `value` is
             instead a dict, this will have multiple elements, depending on the number of elements in the dict.
 
         """
-        pass
+
+        # Figure out how to interpret `value` by checking if it's a str or dict, and then iterate over call to
+        # process each individual tarball
+        l_summary_write_output: List[SummaryWriteOutput]
+        if isinstance(value, str):
+            l_summary_write_output = [self._summarize_results_tarball(value, rootdir, tag=None)]
+        elif isinstance(value, dict):
+            l_summary_write_output = []
+            for sub_key, sub_value in value.items():
+                l_summary_write_output.append(self._summarize_results_tarball(sub_value, rootdir, tag=sub_key))
+        else:
+            raise ValueError("Value in manifest is of unrecognized type.\n"
+                             f"Value was: {value}\n"
+                             f"Type was: {type(value)}")
+
+        return l_summary_write_output
+
+    def _summarize_results_tarball(self, results_tarball_filename, rootdir, tag=None):
+        """Writes summary markdown files for the test results contained in a tarball of the test results product and
+        associated data.
+
+        Parameters
+        ----------
+        results_tarball_filename : str
+            The filename of a tarball containing the test results product and associated datafiles
+        rootdir : str
+        tag : str or None
+            If provided, this tag will be appended to the names of all test cases in the returned data, for purposes of
+            separately labelling different instances of the same test.
+
+        Returns
+        -------
+        summary_write_output : SummaryWriteOutput
+            An object containing the test name and filename and a list of the same for associated tests.
+        """
+
+        qualified_tmpdir = self._make_tmpdir(results_tarball_filename, rootdir)
+
+        # We use a try-finally block here to ensure the created tmpdir is removed after use
+        try:
+            qualified_results_tarball_filename = os.path.join(rootdir, DATA_DIR, results_tarball_filename)
+            summary_write_output = self._summarize_results_tarball_with_tmpdir(qualified_results_tarball_filename,
+                                                                               qualified_tmpdir,
+                                                                               tag=tag)
+        finally:
+            shutil.rmtree(qualified_tmpdir)
+
+        return summary_write_output
+
+    @staticmethod
+    def _make_tmpdir(results_tarball_filename, rootdir):
+        """We'll need a temporary directory to extract files into, so create one. To minimize the risk of clashes in
+        case of future parallelization, we name it via hashing the filename
+
+        Parameters
+        ----------
+        results_tarball_filename : str
+        rootdir : str
+
+        Returns
+        -------
+        qualified_tmpdir : str
+            The fully-qualified path to a tmpdir created for use by this object
+
+        """
+
+        tmpdir = "tmp_" + hash_any(results_tarball_filename, max_length=TMPDIR_MAXLEN)
+
+        # If this already exists, raise an exception - better to fail then to run into unexpected results from thread
+        # clashes
+        qualified_tmpdir = os.path.join(rootdir, tmpdir)
+        os.makedirs(qualified_tmpdir, exist_ok=False)
+
+        return qualified_tmpdir
+
+    def _summarize_results_tarball_with_tmpdir(self,
+                                               qualified_results_tarball_filename,
+                                               qualified_tmpdir,
+                                               tag=None):
+        """Writes summary markdown files for the test results contained in a tarball of the test results product and
+        associated data, using a provided tmpdir for work.
+
+        Parameters
+        ----------
+        qualified_results_tarball_filename : str
+            The fully-qualified filename of a tarball containing the test results product and associated datafiles
+        qualified_tmpdir : str
+            The fully-qualified path to a temporary directory which can be used for this function.
+        tag : str or None
+
+        Returns
+        -------
+        summary_write_output : SummaryWriteOutput
+        """
+
+        assert os.path.isdir(qualified_tmpdir)
+        assert os.path.isfile(qualified_results_tarball_filename)

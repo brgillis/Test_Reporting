f834b0ada630ba64ee6518b68c57c217e3128258
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2022-10-17 13:47:15 +0100

    Adding check of l_test_results in output

--- tests/product_parsing_test.py
+++ tests/product_parsing_test.py
@@ -21,9 +21,10 @@ Unit tests of parsing XML files.
 # the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
 import os
+from datetime import datetime
 
 from utility.constants import TEST_DATA_DIR
-from utility.product_parsing import parse_xml_product
+from utility.product_parsing import SingleTestResult, parse_xml_product
 
 TEST_XML_FILENAME = "she_observation_cti_gal_validation_test_results_product.xml"
 
@@ -40,3 +41,15 @@ def test_parse_xml_product(rootdir):
     qualified_xml_filename = os.path.join(rootdir, TEST_DATA_DIR, TEST_XML_FILENAME)
 
     test_results = parse_xml_product(qualified_xml_filename)
+
+    # We'll check a few random bits of the product are set up correctly, including those most likely to have failed
+
+    # Check the creation date
+    assert (isinstance(test_results.creation_date, datetime))
+    assert test_results.creation_date.year == 2021
+    assert test_results.creation_date.second == 43
+
+    # Check the test results list
+    assert len(test_results.l_test_results) == 24
+    test_results_0 = test_results.l_test_results[0]
+    assert isinstance(test_results_0, SingleTestResult)

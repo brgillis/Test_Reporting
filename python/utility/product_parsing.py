0f9c9c62a7715a3bbcda0182fb153d022d16a0f3
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2022-10-20 16:12:23 +0100

    Refactoring to construct more simply

--- python/utility/product_parsing.py
+++ python/utility/product_parsing.py
@@ -22,10 +22,10 @@ written to output Markdown files.
 # You should have received a copy of the GNU Lesser General Public License along with this library; if not, write to
 # the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
-from dataclasses import Field, dataclass, field
+from dataclasses import dataclass, field
 from datetime import datetime
 from logging import getLogger
-from typing import Any, Dict, List, Literal, Optional, TypeVar
+from typing import List, Optional
 from xml.etree import ElementTree
 from xml.etree.ElementTree import Element
 
@@ -87,14 +87,54 @@ class SupplementaryInfo:
     info_description: str
     info_value: str
 
+    @classmethod
+    def make_from_element(cls, e):
+        """Construct an instance of this class from a corresponding XML element.
+
+        Parameters
+        ----------
+        e : Element
+
+        Returns
+        -------
+        SupplementaryInfo
+        """
+        return SupplementaryInfo(info_key=_element_find(e, "Key", output_type=str),
+                                 info_description=_element_find(e, "Description", output_type=str),
+                                 info_value=_element_find(e, "StringValue", output_type=str),
+                                 )
+
 
 @dataclass
 class RequirementResults:
     req_id: str
-    meas_value: Any
-    req_result: Literal["PASSED", "FAILED"]
+    meas_value: str
+    req_result: str
     req_comment: Optional[str] = None
     l_supp_info: List[SupplementaryInfo] = field(default_factory=list)
+
+    @classmethod
+    def make_from_element(cls, e):
+        """Construct an instance of this class from a corresponding XML element.
+
+        Parameters
+        ----------
+        e : Element
+
+        Returns
+        -------
+        RequirementResults
+        """
+
+        l_supp_info = [SupplementaryInfo.make_from_element(sub_e) for sub_e in
+                       _element_find(e, "Requirement.SupplementaryInformation.Parameter", find_all=True)]
+
+        return RequirementResults(req_id=_element_find(e, "Requirement.Id", output_type=str),
+                                  meas_value=_element_find(e, "Requirement.MeasuredValue", output_type=str),
+                                  req_result=_element_find(e, "Requirement.ValidationResult", output_type=str),
+                                  req_comment=_element_find(e, "Requirement.Comment", output_type=str),
+                                  l_supp_info=l_supp_info,
+                                  )
 
 
 @dataclass
@@ -103,6 +143,24 @@ class AnalysisResult:
     textfiles_tarball: Optional[str] = None
     figures_tarball: Optional[str] = None
     ana_comment: Optional[str] = None
+
+    @classmethod
+    def make_from_element(cls, e):
+        """Construct an instance of this class from a corresponding XML element.
+
+        Parameters
+        ----------
+        e : Element
+
+        Returns
+        -------
+        AnalysisResult
+        """
+        return AnalysisResult(ana_result=_element_find(e, "Result", output_type=str),
+                              textfiles_tarball=_element_find(e, "AnalysisFiles.TextFiles.FileName", output_type=str),
+                              figures_tarball=_element_find(e, "AnalysisFiles.Figures.FileName", output_type=str),
+                              ana_comment=_element_find(e, "Comment", output_type=str),
+                              )
 
 
 @dataclass
@@ -115,6 +173,30 @@ class SingleTestResult:
 
     l_requirements: List[RequirementResults] = field(default_factory=list)
     analysis_result: Optional[AnalysisResult] = None
+
+    @classmethod
+    def make_from_element(cls, e):
+        """Construct an instance of this class from a corresponding XML element.
+
+        Parameters
+        ----------
+        e : Element
+
+        Returns
+        -------
+        SingleTestResult
+        """
+
+        l_requirements = [RequirementResults.make_from_element(sub_e) for sub_e in
+                          _element_find(e, "ValidatedRequirements", find_all=True)]
+        analysis_result = AnalysisResult.make_from_element(_element_find(e, "AnalysisResult"))
+
+        return SingleTestResult(test_id=_element_find(e, "TestId", output_type=str),
+                                test_description=_element_find(e, "TestDescription", output_type=str),
+                                global_result=_element_find(e, "GlobalResult", output_type=str),
+                                l_requirements=l_requirements,
+                                analysis_result=analysis_result,
+                                )
 
 
 @dataclass
@@ -140,13 +222,41 @@ class TestResults:
     # Data
     l_test_results: List[SingleTestResult] = field(default_factory=list)
 
+    @classmethod
+    def make_from_element(cls, e):
+        """Construct an instance of this class from a corresponding XML element.
 
-S_XML_OBJECT_TYPES = {TestResults, SingleTestResult, RequirementResults, AnalysisResult, SupplementaryInfo}
+        Parameters
+        ----------
+        e : Element
 
-OutputType = TypeVar("OutputType")
+        Returns
+        -------
+        TestResults
+        """
+
+        l_test_results = [SingleTestResult.make_from_element(sub_e) for sub_e in
+                          _element_find(e, "Data.ValidationTestList", find_all=True)]
+        creation_date = _construct_datetime(_element_find(e, "Header.CreationDate", output_type=str))
+
+        return TestResults(product_id=_element_find(e, "Header.ProductId", output_type=str),
+                           dataset_release=_element_find(e, "Header.DataSetRelease", output_type=str),
+                           plan_id=_element_find(e, "Header.PlanId", output_type=str),
+                           ppo_id=_element_find(e, "Header.PPOId", output_type=str),
+                           pipeline_definition_id=_element_find(e, "Header.PipelineDefinitionId", output_type=str),
+                           creation_date=creation_date,
+                           exp_product_id=_element_find(e, "Data.ExposureProductId", output_type=str),
+                           obs_id=_element_find(e, "Data.ObservationId", output_type=int),
+                           pnt_id=_element_find(e, "Data.PointingId", output_type=int),
+                           obs_mode=_element_find(e, "Data.ObservationMode", output_type=str),
+                           n_exp=_element_find(e, "Data.NumberExposures", output_type=int),
+                           tile_id=_element_find(e, "Data.TileId", output_type=int),
+                           source_pipeline=_element_find(e, "Data.SourcePipeline", output_type=str),
+                           l_test_results=l_test_results,
+                           )
 
 
-def recursive_element_find(element, tag, find_all=False):
+def _element_find(element, tag, find_all=False, output_type=None):
     """Gets a sub-element from an XML ElementTree Element, searching recursively as necessary.
 
     Parameters
@@ -158,26 +268,42 @@ def recursive_element_find(element, tag, find_all=False):
     find_all : bool, default=False
         If False, will return the results of `element.find` in final step, which returns a single value. If True,
         will return the results of `element.findall` in the final step, which returns a list of all values.
+    output_type : None or Type[str] or Type[int]
+        If None, will return the Element directly. If str or int, will convert to this type if the Element is None,
+        and will return None if it is None.
 
     Returns
     -------
-    sub_element : Element
-        The sub-element for the given tag
+    Element or str or int or List[Element] or List[str] or List[int]
+        The sub-element for the given tag, or else the str or int representation of it if requested
     """
 
     # Check for simple case, to break out of recursion
     if "." not in tag:
         if find_all:
-            return element.findall(tag)
+            l_output = element.findall(tag)
+            if output_type is str:
+                l_output = [_e_to_str(output) for output in l_output]
+            elif output_type is int:
+                l_output = [_e_to_int(output) for output in l_output]
+            return l_output
         else:
-            return element.find(tag)
+            try:
+                output = element.find(tag)
+            except Exception as e:
+                print(str(e))
+            if output_type is str:
+                output = _e_to_str(output)
+            elif output_type is int:
+                output = _e_to_int(output)
+            return output
 
     # Otherwise, split on the first . and call this method recursively
     head, tail = tag.split(".", maxsplit=1)
-    return recursive_element_find(element.find(head), tail, find_all=find_all)
+    return _element_find(element.find(head), tail, find_all=find_all, output_type=output_type)
 
 
-def construct_datetime(s):
+def _construct_datetime(s):
     """Converts a string value, formatted like "YYYY-MM-DDTHH:MM:SS.408Z", into a datetime object.
 
     Parameters
@@ -196,86 +322,36 @@ def construct_datetime(s):
     return dt
 
 
-def get_constructor(output_type):
-    """Get a constructor that can be called on a str to convert it to the desired output type. This works by checking
-    first for known types which need special handling, and then defaulting to using the type itself as a constructor.
+def _e_to_str(e):
+    """Tries to convert an XML element to a string. Returns None if None is provided
 
     Parameters
     ----------
-    output_type : Type[OutputType]
-        The type of the object to be generated.
+    e : Element or None
 
     Returns
     -------
-    constructor : Callable[[str], OutputType]
-        An appropriate constructor for this type
-
+    str or None
     """
-
-    if output_type == datetime:
-        return construct_datetime
-
-    return output_type
-
-
-def create_from_xml_element(output_type, element):
-    """Generates an object of the desired output type from an element in an XML ElementTree.
-
-    Parameters
-    ----------
-    output_type : Type[OutputType]
-        The type of the object to be generated.
-    element : Element
-        The element in an XML ElementTree from which to generate the output object
-
-    Returns
-    -------
-    output_object : OutputType
-        The generated object.
-    """
-
-    if element is None:
+    if e is None:
         return None
+    return e.text
 
-    if hasattr(output_type, "__args__"):
-        base_output_type = output_type.__args__[0]
-    else:
-        base_output_type = output_type
 
-    # If the desired type is not one of the XML object types, simply read in the value from the element
-    if base_output_type not in S_XML_OBJECT_TYPES:
-        try:
-            constructor = get_constructor(base_output_type)
-            output = constructor(element.text)
-        except Exception as e:
-            logger.warning("Could not convert value %s to type %s; will be stored as a string. Exception was: %s",
-                           element.text, base_output_type, e)
-            output = str(element.text)
-        return output
+def _e_to_int(e):
+    """Tries to convert an XML element to an int. Returns None if None is provided
 
-    # We use the type's meta-info to find what attributes it has and their types, and use this to recursively
-    # construct it
-    d_attrs: Dict[str, Any] = {}
-    attr_name: str
-    dataclass_field: Field
-    for attr_name, dataclass_field in base_output_type.__dataclass_fields__.items():
+    Parameters
+    ----------
+    e : Element or None
 
-        attr_type = dataclass_field.type
-        attr_tag: str = getattr(Tags, attr_name)
-
-        # Special handling for lists
-        if dataclass_field.default_factory == list:
-            attr_elem_type = attr_type.__args__[0]
-
-            l_sub_elements = recursive_element_find(element, attr_tag, find_all=True)
-            d_attrs[attr_name] = [create_from_xml_element(attr_elem_type, sub_element)
-                                  for sub_element in l_sub_elements]
-
-        else:
-            sub_element = recursive_element_find(element, attr_tag)
-            d_attrs[attr_name] = create_from_xml_element(attr_type, sub_element)
-
-    return base_output_type(**d_attrs)
+    Returns
+    -------
+    int or None
+    """
+    if e is None:
+        return None
+    return int(e.text)
 
 
 def parse_xml_product(filename):
@@ -294,8 +370,6 @@ def parse_xml_product(filename):
 
     tree = ElementTree.parse(filename)
 
-    root = tree.getroot()
-
-    test_results = create_from_xml_element(TestResults, root)
+    test_results = TestResults.make_from_element(tree.getroot())
 
     return test_results

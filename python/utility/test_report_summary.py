ed84e1117e13b81c9c1871ffc656a6b1f9686434
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2022-11-04 15:36:12 +0000

    Fixing test

--- python/utility/test_report_summary.py
+++ python/utility/test_report_summary.py
@@ -75,7 +75,7 @@ def build_test_report_summary(test_report_summary_filename,
                      f"| {test_meta.num_passed} "
                      f"| {test_meta.num_failed} |\n")
 
-        fo.write("The log file for building these test reports can be found [here](build.log).")
+        fo.write("\nThe log file for building these test reports can be found [here](build.log).")
 
 
 @log_entry_exit(logger)

ba691b230cfffe12a2585917a360f3d21bd3e1b3
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2022-11-22 16:12:27 +0000

    Setting up basic integration test of standalone executable

--- tests/integration_test.py
+++ tests/integration_test.py
@@ -22,12 +22,16 @@ Unit tests of running the whole script in a minimal state.
 
 import os
 
-from Test_Reporting import build_all_report_pages
-from Test_Reporting.utility.constants import PUBLIC_DIR, TEST_REPORT_SUMMARY_FILENAME
+from Test_Reporting.testing.common import TEST_TARBALL_FILENAME
+
+from Test_Reporting.specialization_keys import CTI_GAL_KEY
+
+from Test_Reporting import build_all_report_pages, build_report
+from Test_Reporting.utility.constants import DATA_DIR, PUBLIC_DIR, TEST_REPORT_SUMMARY_FILENAME
 
 
-def test_integration(project_copy, test_manifest):
-    """Tests a slimmed-down full execution of the build script, using the default implementation.
+def test_build_all_integration(project_copy, test_manifest):
+    """Tests a slimmed-down full execution of the build_all script, using the default implementation.
 
     Parameters
     ----------
@@ -58,7 +62,6 @@ def test_cti_gal_integration(project_copy, cti_gal_manifest):
     Parameters
     ----------
     project_copy : str
-        Pytest fixture providing the fully-qualified path to a copy of the project created for testing purposes.
     cti_gal_manifest : str
         Pytest fixture providing the fully-qualified path to the manifest used for testing the CTI-Gal builder
     """
@@ -71,6 +74,33 @@ def test_cti_gal_integration(project_copy, cti_gal_manifest):
 
     # Call the main workhorse function
     build_all_report_pages.run_build_all_from_args(args)
+
+    # Check that output looks as expected
+
+    qualified_test_report_summary_filename = os.path.join(project_copy, PUBLIC_DIR, TEST_REPORT_SUMMARY_FILENAME)
+    assert os.path.isfile(qualified_test_report_summary_filename)
+
+
+def test_standalone_integration(project_copy, tmpdir_factory):
+    """Tests a full execution of the standalone build script.
+
+    Parameters
+    ----------
+    project_copy : str
+    tmpdir_factory : TempdirFactory
+        Pytest fixture providing a factory to create temporary directories for testing.
+    """
+
+    # Set up the mock arguments
+    parser = build_report.get_build_argument_parser()
+    args = parser.parse_args([])
+    args.target = os.path.join(project_copy, DATA_DIR, TEST_TARBALL_FILENAME)
+    args.datadir = project_copy
+    args.reportdir = str(tmpdir_factory.mktemp("project_copy"))
+    args.key = CTI_GAL_KEY
+
+    # Call the main workhorse function
+    build_report.run_build_from_args(args)
 
     # Check that output looks as expected
 

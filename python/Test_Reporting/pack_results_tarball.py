b705b20f8b4661e762a57a02979b3c32f886201c
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2022-12-05 17:07:07 +0000

    Filling out functions for pack_results_tarball.py

--- python/Test_Reporting/pack_results_tarball.py
+++ python/Test_Reporting/pack_results_tarball.py
@@ -9,7 +9,7 @@
 This python script is run to collect a validation test results product (or listfile thereof) and all its associated
 datafiles into a tarball which can be easily moved into this project for publishing.
 """
-
+import json
 # Copyright (C) 2012-2020 Euclid Science Ground Segment
 #
 # This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General
@@ -34,6 +34,7 @@ from Test_Reporting.utility.constants import JSON_EXT, TARBALL_EXT, XML_EXT
 from Test_Reporting.utility.misc import (get_qualified_path, get_relative_path, is_valid_json_filename,
                                          is_valid_xml_filename,
                                          log_entry_exit, tar_files, )
+from Test_Reporting.utility.product_parsing import parse_xml_product
 
 if TYPE_CHECKING:
     import Namespace  # noqa F401
@@ -158,6 +159,67 @@ def run_pack_from_args(args):
               l_filenames=l_files_to_pack,
               workdir=args.workdir,
               delete_files=False)
+
+
+@log_entry_exit(logger)
+def get_l_files_to_pack_from_listfile(listfile_filename, workdir):
+    """Parses a `.json` listfile and the files it points to, getting a list of all these files so that they can be
+    packed into a tarball.
+
+    Parameters
+    ----------
+    listfile_filename : str
+        The `.json` listfile filename relative to `workdir`.
+    workdir : str
+        The work directory in which all files are stored.
+
+    Returns
+    -------
+    l_files_to_pack : List[str]
+        List of filenames of files to be packed, relative to `workdir`.
+    """
+
+    # Read in the listfile
+    qualified_listfile_filename = os.path.join(workdir, listfile_filename)
+    with open(qualified_listfile_filename, "r") as fi:
+        l_product_filenames = json.load(fi)
+
+    # Combine the files for each product this listfile points to
+    l_files_to_pack = [listfile_filename]
+    for product_filename in l_product_filenames:
+        l_files_to_pack += get_l_files_to_pack_from_product(product_filename=product_filename,
+                                                            workdir=args.workdir)
+
+    return l_files_to_pack
+
+
+@log_entry_exit(logger)
+def get_l_files_to_pack_from_product(product_filename, workdir):
+    """Parses a `.xml` data product and the files it points to, getting a list of all these files so that they can be
+    packed into a tarball.
+
+    Parameters
+    ----------
+    product_filename : str
+        The `.xml` product filename relative to `workdir`.
+    workdir : str
+        The work directory in which all files are stored.
+
+    Returns
+    -------
+    l_files_to_pack : List[str]
+        List of filenames of files to be packed, relative to `workdir`.
+    """
+
+    # Start off by including this product's filename in the output list
+    l_files_to_pack = [product_filename]
+
+    # Read the product into memory, then get all filenames from it
+    product = parse_xml_product(os.path.join(workdir, product_filename))
+    l_files_to_pack += [tr.analysis_result.textfiles_tarball for tr in product.l_test_results]
+    l_files_to_pack = [tr.analysis_result.figures_tarball for tr in product.l_test_results]
+
+    return l_files_to_pack
 
 
 if __name__ == "__main__":

b0f13963d5f6dcffe6f0dfd15cb8dd82dec24335
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2023-01-03 15:31:11 +0000

    Clarifying string
    Former-commit-id: 4b0d9ea58315e4174c7e2ea19951237e906ce684

--- python/Test_Reporting/specializations/cti_gal.py
+++ python/Test_Reporting/specializations/cti_gal.py
@@ -98,10 +98,10 @@ class CtiGalReportSummaryWriter(ReportSummaryWriter):
         # If we have any error messages, print them out
         if l_slope_err_str:
             for line in l_slope_err_str:
-                writer.add_line(f"**ERROR:** {line}\n\n")
+                writer.add_line(f"**Error reported:** {line}\n\n")
         if l_int_err_str:
             for line in l_int_err_str:
-                writer.add_line(f"**ERROR:** {line}\n\n")
+                writer.add_line(f"**Error reported:** {line}\n\n")
 
         self._add_cti_gal_binned_details(writer=writer,
                                          test_case_results=test_case_results,
